c2ec3a685f21703d37529fdf61bc538d3f5a90cb
snehalamr111 <snehalamr111@gmail.com>
2023-08-10 01:37:50 +0530

    third commit

--- src/main/java/com/pages/LoginPage.java
+++ src/main/java/com/pages/LoginPage.java
@@ -4,5 +4,6 @@ public class LoginPage {
 	public void loginpage() {
 		System.out.println("This is login page");
 		System.out.println("This is first comment");
+		System.out.println("This is third comment");
 	}
 }
